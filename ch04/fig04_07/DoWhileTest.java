825172fb4c1743ba0c156369150ecc9cd5165766
Nguyen Viet Tuan <tuan.nv@teko.vn>
2019-10-24 12:23:23 +0700

    do ... while loop

---
+++ ch04/fig04_07/DoWhileTest.java
@@ -0,0 +1,10 @@
+public class DoWhileTest {
+    public static void main(String[] args) {
+        int counter = 1;
+        do {
+            System.out.printf("%d%n", counter);
+            counter++;
+        } while (counter <= 10);
+        System.out.println();
+    }
+}
