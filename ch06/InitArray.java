808e01f75e0af2749e2a3900e02306c363c15f3d
Nguyen Viet Tuan <tuan.ngviet@gmail.com>
2019-10-29 15:25:35 +0700

    6.12 Using Command-Line Arguments

--- ch06/InitArray.java
+++ ch06/InitArray.java
@@ -1,11 +1,19 @@
 
 public class InitArray {
     public static void main(String[] args) {
-        final int ARRAY_LENGTH = 10;
-        int[] array = new int[ARRAY_LENGTH];
+        if (args.length != 3) {
+            System.out.println("Error, please specify 3 arguments: array length, initial value, increment");
+            return;
+        }
 
-        for (int counter = 0; counter < array.length; counter++)
-            array[counter] = 2 + 2 * counter;
+        final int ARRAY_LENGTH = Integer.parseInt(args[0]);
+        int[] array = new int[ARRAY_LENGTH];
+        array[0] = Integer.parseInt(args[1]);
+        int increment = Integer.parseInt(args[2]);
+
+        // skip the first element, since it's the args[1] we mentioned above
+        for (int counter = 1; counter < array.length; counter++)
+            array[counter] = array[0] + increment * counter;
 
         System.out.printf("%s%8s%n", "Index", "Value");
         for (int counter = 0; counter < array.length; counter++) {
