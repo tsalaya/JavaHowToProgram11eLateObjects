5ea40d1ce941a43d734ad9cc9121ffec0aae16a2
Nguyen Viet Tuan <tuan.ngviet@gmail.com>
2019-11-07 16:45:05 +0700

    begin 9.4.3 Creating a CommissionEmployee–BasePlusCommissionEmployee Inheritance Hierarchy

---
+++ ch09/fig09_04/BasePlusCommissionEmployeeTest.java
@@ -0,0 +1,9 @@
+public class BasePlusCommissionEmployeeTest {
+    public static void main(String[] args) {
+        BasePlusCommissionEmployee employee = new BasePlusCommissionEmployee("Bob", "Lewis", "333-33-3333", 5000, .04,
+                300);
+
+        System.out.println("Employee information obtained by get methods:");
+        System.out.printf("%n%s: %n%n%s%n", "Updated information obtained by toString()", employee);
+    }
+}
