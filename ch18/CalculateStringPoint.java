7caa23efc5187d1fa2ed5de64531a580f5f47b23
Nguyen Viet Tuan <tuan.ngviet@gmail.com>
2019-12-17 09:00:35 +0700

    CalculateStringPoint.java

---
+++ ch18/CalculateStringPoint.java
@@ -0,0 +1,34 @@
+import java.util.Scanner;
+
+public class CalculateStringPoint {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("Enter a string: ");
+        String str = input.nextLine();
+
+        int total = calculateStringPointRepetitive(str);
+        int total1 = calculateStringPointRecursive(str);
+
+        System.out.printf("Total is %d (repetitive)%n", total);
+        System.out.printf("Total is %d (recursive)%n", total1);
+
+        input.close();
+    }
+
+    public static int calculateStringPointRecursive(String s) {
+        if (s.length() == 1)
+            return (int) s.toCharArray()[0] - 96;
+        else
+            return calculateStringPointRecursive(s.substring(0, 1))
+                    + calculateStringPointRecursive(s.substring(1, s.length()));
+    }
+
+    public static int calculateStringPointRepetitive(String s) {
+        int total = 0;
+        for (char c : s.toCharArray()) {
+            total += (int) c - 96;
+        }
+        return total;
+    }
+}
